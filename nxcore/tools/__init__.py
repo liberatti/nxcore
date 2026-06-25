4c3d17f3cfd9b0f2ebe4648d863a6841db66e84f
Gustavo Liberatti <liberatti.gustavo@gmail.com>
2026-06-25 08:49:01 -0400

    refactor: remove redundant package exports from repository and tools initialization modules

--- nxcore/tools/__init__.py
+++ nxcore/tools/__init__.py
@@ -1,12 +1,1 @@
 """Tools module containing integration utilities for OAuth and image handling."""
-
-from .google_oauth import GoogleOAuth, GoogleOauth
-from .microsoft_oauth import MicrosoftOAuth
-from .image_tool import ImageTool
-
-__all__ = [
-    "GoogleOAuth",
-    "GoogleOauth",
-    "MicrosoftOAuth",
-    "ImageTool",
-]
